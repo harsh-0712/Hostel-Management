c12671b61ae5c26cc271b42e765b09537a894b4b
Mahendra Singh <07harsh12solanki@gmail.com>
2023-07-06 11:35:28 +0530

    Working out Servers

--- helper/server.js
+++ helper/server.js
@@ -16,7 +16,17 @@ app.get('/',(req,res)=>{
         axios.get('https://hm-server-2tgn.onrender.com/helper')
         console.log('made req');
         // axios.get('http://localhost:8080/helper')
-    },120000)
+    },600000)
+    setInterval(async()=>{
+        console.log('calling helper 2');
+        axios.get('https://helper-service.onrender.com/help')
+        // axios.get('http://localhost:8080/helper')
+    },600000)
+    return res.status(201).json("Helper")
+})
+
+app.get('/help',(req,res)=>{
+    console.log('help');
     return res.status(201).json("Helper")
 })
 
